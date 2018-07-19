f52efa2a1d77a26c4fd2d546f90154fc9d68cc28
saud maruli <uthrul@gmail.com>
2018-07-19 15:14:09 +0700

    create login and logo app with component textinput,Image, Dimensions, KeyboardAvoidingView,

---
+++ components/Login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {  
+    View, Text, StyleSheet, 
+    Image, Dimensions, KeyboardAvoidingView,
+
+} from 'react-native';
+import Myform from './MyForm';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+            <Image 
+                style={styles.logo}
+                source={require('./images/logo.png')}
+            />
+            <Text style={styles.title}>Noize Code app Login</Text>
+        </View>
+        <View style={styles.myForm}>
+                <Myform/>
+            </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#282830',
+        width: Dimensions.get('window').width,
+
+    },
+    logoContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: '#fff'
+    },
+    logo: {
+        width: 250,
+        height: 70,
+    },
+    myForm:{
+        flex:1,
+    }
+  });
---
+++ components/MyForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {  
+    View, Text, TextInput, 
+    TouchableOpacity, StyleSheet
+} from 'react-native';
+
+export default class Myform extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput 
+            placeholder="username"
+            style={styles.Input}
+        />
+        <TextInput 
+            placeholder="password"
+            style={styles.Input}
+        />
+        <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttontext}>Let's Go</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    Input: {
+        height: 40,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        paddingLeft: 20,
+        marginBottom: 10,
+    },
+    buttonContainer:{
+        backgroundColor: '#27ae69',
+        paddingVertical: 16,
+    },
+    buttontext:{
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold'
+    }
+  });
